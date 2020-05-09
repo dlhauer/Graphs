42ba01c2c25e5d092a4c1443d5692b4d7d0af0c1
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-09 15:13:13 -0700

    completes dft_recursive

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -44,13 +44,17 @@ class Graph:
                     visited.add(v)
 
     def dft_recursive(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
+        visited = {starting_vertex}
 
-        This should be done using recursion.
-        """
-        pass  # TODO
+        def dft_print(starting_vertex):
+            print(starting_vertex)
+            for v in self.get_neighbors(starting_vertex):
+                if v not in visited:
+                    visited.add(v)
+                    dft_print(v)
+            
+
+        dft_print(starting_vertex)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -103,7 +107,7 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)
+    # print(graph.vertices)
 
     '''
     Valid BFT paths:
@@ -120,7 +124,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    graph.bft(1)
+    # graph.bft(1)
 
     '''
     Valid DFT paths:
@@ -129,19 +133,19 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    graph.dft(1)
+    # graph.dft(1)
     graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    # print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
-    print(graph.dfs_recursive(1, 6))
+    # print(graph.dfs(1, 6))
+    # print(graph.dfs_recursive(1, 6))
